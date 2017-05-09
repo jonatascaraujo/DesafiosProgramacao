5bd33fc5a72d9fbaf9d46cde89b93fddb439cd3a
Jônatas C. Araújo <jonatascaraujo@gmail.com>
2017-05-09 20:07:38 -0300

    Start p100

---
+++ p100/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class Main {
+	
+	static Map<Integer, Integer> mapaNumeroCiclicos= new HashMap<Integer, Integer>();
+	
+	public static void main(String args[]) {
+		int menor, maior;
+		Scanner scan = new Scanner(System.in);
+		while (scan.hasNext()) {
+			menor = scan.nextInt();
+			maior = scan.nextInt();
+			if(maior<menor){
+				int temp = maior;
+				maior=menor;
+				menor =temp;
+			}
+			int tamanhoCiclico = executaAlgoritmo(menor, maior);
+			System.out.println(menor + " " + maior + " " + tamanhoCiclico);
+			scan.nextLine();
+		}
+		scan.close();
+	}
+
+	static int executaAlgoritmo(int menor, int maior) {
+		int maiorTamanhoCiclico = 0;
+		for (int i = menor; i <= maior; i++) {
+			int tamanhoCiclico = executaConjectura(i);
+			maiorTamanhoCiclico = maiorTamanhoCiclico<tamanhoCiclico?tamanhoCiclico:maiorTamanhoCiclico;
+		}
+		return maiorTamanhoCiclico;
+	}
+
+	
+	static int executaConjectura(int n) {
+		int ciclico = 1;
+		if (mapaNumeroCiclicos.containsKey(n)) {
+			return mapaNumeroCiclicos.get(n);
+		}
+		if (n != 1) {
+			ciclico += executaConjectura((n % 2 == 1) ? (3 * n + 1) : (n / 2));
+		}
+		mapaNumeroCiclicos.put(n, ciclico);
+		return ciclico;
+	}
+
+}
